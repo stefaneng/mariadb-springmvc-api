b657494cfedcebb9e9e2addc6a1920482d5f3457
Stefan Eng <stefaneng@ucla.edu>
2016-12-19 14:36:37 -0800

    Empty user class and api route to get a list of users

--- src/main/java/com/stefanengineering/ApiController.java
+++ src/main/java/com/stefanengineering/ApiController.java
@@ -1,12 +1,21 @@
 package com.stefanengineering;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
+
     @RequestMapping("/")
     public String rootPage() {
         return "Example";
+    }
+
+    @RequestMapping("/api/users")
+    public Collection<User> listUsers() {
+        return new LinkedList<>();
     }
 }
