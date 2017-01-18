0b0c2558df17ef67e583a87b38e6c14f577dfc82
Stefan Eng <stefaneng@ucla.edu>
2017-01-17 16:39:24 -0800

    Remove commented out init code

--- src/main/java/com/stefanengineering/Application.java
+++ src/main/java/com/stefanengineering/Application.java
@@ -2,10 +2,8 @@ package com.stefanengineering;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,21 +11,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner initRepository(UserRepository repository) {
-        return (args) -> {
-            /*
-             * repository.save(new User("Stefan", "Eng")); repository.save(new
-             * User("John", "Smith")); repository.save(new User("Alex",
-             * "Johnson"));
-             * 
-             * logger.info("Customers found with findAll():");
-             * logger.info("-------------------------------"); for (User
-             * customer : repository.findAll()) {
-             * logger.info(customer.toString()); } logger.info("");
-             */
-        };
     }
 }
