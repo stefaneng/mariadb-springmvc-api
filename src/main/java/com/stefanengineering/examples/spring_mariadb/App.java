b43b5f275ec17abd7fe16428016d77e32f619a2a
Stefan Eng <stefaneng@ucla.edu>
2017-01-18 11:42:52 -0800

    Use spring-boot autoconfiguration
    Cleanup code and rename spring.version to springboot.version.

--- src/main/java/com/stefanengineering/examples/spring_mariadb/App.java
+++ src/main/java/com/stefanengineering/examples/spring_mariadb/App.java
@@ -1,13 +1,11 @@
 package com.stefanengineering.examples.spring_mariadb;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootConfiguration
-public class App 
-{
-    public static void main( String[] args )
-    {
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
