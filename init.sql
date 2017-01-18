4c68099f5f600e69975d958c6b4693105543ff39
Stefan Eng <stefaneng@ucla.edu>
2017-01-17 16:50:46 -0800

    Remove all old files

--- init.sql
+++
@@ -1,4 +0,0 @@
-drop database if exists springJpaExample;
-create database springJpaExample;
-use springJpaExample;
-create table user (id INT PRIMARY KEY AUTO_INCREMENT, first_name text NOT NULL, last_name text NOT NULL);
